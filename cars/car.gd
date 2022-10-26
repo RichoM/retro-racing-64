ab8712f8393da8bed344c4c38ab8a8ae2e876c29
RichoM <richi.moran@gmail.com>
2022-10-26 20:59:26 -0300

    I finally got an acceptable car control.
    I still have to tweak a few parameters and magic numbers. And also the steering doesn't seem to work the same when lowering the FPS but at least it feels nice to drive.

--- cars/car.gd
+++ cars/car.gd
@@ -13,12 +13,15 @@ onready var wheel_front_right = $mesh/body/wheel_front_right
 onready var wheels = [wheel_back_left, wheel_back_right,
 					wheel_front_left, wheel_front_right]
 					
+var ball_speed = 0
+					
 var sphere_offset = Vector3(0, -1.5, 0)
 var speed_input = 0
+var rotate_input = 0
 
 var acceleration = 0.9
-var steering = 25
-var turn_speed = 2.5
+var steering = 30
+var turn_speed = 3.00
 var turn_stop_limit = 0.75
 var body_tilt = 135
 
@@ -31,49 +34,64 @@ func _ready():
 	ground_ray.add_exception(ball)
 
 func _process(delta):
+	ball_speed = ball.linear_velocity.length()
+	
+	if camera:
+		var b : RigidBody = ball
+		camera.h_offset = lerp(camera.h_offset, -rotate_input, 0.5)
+		camera.fov = lerp(50, 100, b.linear_velocity.length() / 50)
+		camera.fov = min(camera.fov, 100)
+	
+	speed_input *= acceleration
+	
 	# Can't steer/accelerate when in the air
-	if not ground_ray.is_colliding():
-		return
+	if not ground_ray.is_colliding(): return
+	
 	# f/b input
 	#speed_input = 0
-	speed_input += Input.get_action_strength("accelerate") * 30
+	speed_input += Input.get_action_strength("accelerate") * 25
 	speed_input -= Input.get_action_strength("brake") * 10
 	
 	# steer input
 #	rotate_target = lerp(rotate_target, rotate_input, 5 * delta)
-	var rotate_input = 0
-	rotate_input += Input.get_action_strength("steer_left")
-	rotate_input -= Input.get_action_strength("steer_right")
-	rotate_input *= deg2rad(steering)
-	
+	#rotate_input = 0
+	var steer_left = Input.get_action_strength("steer_left") * 2.5
+	var steer_right = Input.get_action_strength("steer_right") * 2.5
+	if steer_left == 0 and steer_right == 0:
+		if rotate_input > 0:
+			rotate_input -= deg2rad(2.5)
+			rotate_input = max(rotate_input, 0)
+		elif rotate_input < 0:
+			rotate_input += deg2rad(2.5)
+			rotate_input = min(rotate_input, 0)
+		
+	else:
+		rotate_input += deg2rad(steer_left if speed_input >= 0 else steer_right)
+		rotate_input -= deg2rad(steer_right if speed_input >= 0 else steer_left)
+		rotate_input = clamp(rotate_input, -1.2, 1.2)
+		
 	# rotate wheels for effect
-	wheel_front_right.rotation.z = -rotate_input
-	wheel_front_left.rotation.z = -rotate_input
+	var wheel_rotation = rotate_input * deg2rad(steering)
+	if speed_input < 0: wheel_rotation *= -1
+	wheel_front_right.rotation.z = -wheel_rotation
+	wheel_front_left.rotation.z = -wheel_rotation
 		
 	# rotate car mesh
 	if ball.linear_velocity.length() > turn_stop_limit:
-		var new_basis = car_mesh.global_transform.basis.rotated(car_mesh.global_transform.basis.y, rotate_input)
+		var new_rotation = rotate_input * deg2rad(steering)
+		var new_basis = car_mesh.global_transform.basis.rotated(car_mesh.global_transform.basis.y, new_rotation)
 		car_mesh.global_transform.basis = car_mesh.global_transform.basis.slerp(new_basis, turn_speed * delta)
 		car_mesh.global_transform = car_mesh.global_transform.orthonormalized()
+		
 		
 		# tilt body for effect
 		#var t = -rotate_input * ball.linear_velocity.length() / body_tilt
 		#$mesh/body.rotation.z = lerp($mesh/body.rotation.z, t, 10 * delta)
-		
-	var t = ball.linear_velocity.length()
-	#$mesh/body.rotation.z = lerp($mesh/body.rotation.z, t, 10 * delta)
 	
 	# align mesh with ground normal
 	var n = ground_ray.get_collision_normal()
 	var xform = align_with_y(car_mesh.global_transform, n.normalized())
 	car_mesh.global_transform = car_mesh.global_transform.interpolate_with(xform, 10 * delta)
-	
-	if camera:
-		var b : RigidBody = ball
-		#camera.h_offset = -rotate_input * 1.5
-		#camera.fov = lerp(60, 90, b.linear_velocity.length() / 75)
-		#camera.fov = min(camera.fov, 90)
-		
 	
 func _physics_process(delta):
 	#car_mesh.transform.origin = ball.transform.origin + sphere_offset
@@ -86,7 +104,8 @@ func _physics_process(delta):
 	#car_mesh.transform.origin = lerp(car_mesh.transform.origin, ball.transform.origin + sphere_offset, 0.3)
 	var force = -car_mesh.global_transform.basis.z * speed_input * 20
 	ball.add_central_force(force)
-	speed_input *= acceleration
+	
+	
 
 func align_with_y(xform, new_y):
 	xform.basis.y = new_y
--- test/test.gd
+++ test/test.gd
@@ -1,1 +1,10 @@
 extends Spatial
+
+onready var car = $car
+
+func _process(delta):
+	$GUI/debug_labels/fps.text = "FPS: " + str(Engine.get_frames_per_second())
+	$GUI/debug_labels/speed_input.text = "Speed: " + str(car.speed_input) + ", " + str(car.ball_speed)
+	$GUI/debug_labels/rotate_input.text = "Rotation: " + str(car.rotate_input)
+	$GUI/debug_labels/steer_left.text = "Left: " + str(Input.get_action_strength("steer_left"))
+	$GUI/debug_labels/steer_right.text = "Right: " + str(Input.get_action_strength("steer_right"))
