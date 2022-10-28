4912b5172e15217da0831bc2576e0adc08720ce2
RichoM <richi.moran@gmail.com>
2022-10-28 12:52:51 -0300

    Added a camera animation for the main screen as well as a "play button"

--- test/test.gd
+++ test/test.gd
@@ -1,6 +1,10 @@
 extends Spatial
 
 onready var car = $car
+onready var animation = $AnimationPlayer
+
+func _ready():
+	animation.play("camera")
 
 func _process(delta):
 	$GUI/debug_labels/fps.text = "FPS: " + str(Engine.get_frames_per_second())
@@ -8,3 +12,12 @@ func _process(delta):
 	$GUI/debug_labels/rotate_input.text = "Rotation: " + str(car.rotate_input)
 	$GUI/debug_labels/steer_left.text = "Left: " + str(Input.get_action_strength("steer_left"))
 	$GUI/debug_labels/steer_right.text = "Right: " + str(Input.get_action_strength("steer_right"))
+
+
+func _on_play_button_pressed():
+	animation.stop()
+	$GUI/title.hide()
+	$GUI/play_button.hide()
+	$GUI/onscreen_controls.show()
+	$Camera.current = false
+	car.camera.current = true
