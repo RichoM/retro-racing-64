cbc03f5c275e045ce4c43ab32ab151c20be0e0da
RichoM <richi.moran@gmail.com>
2022-10-29 22:36:05 -0300

    Changed tree placement to prevent trees on the track

--- landscape/random_scatter.gd
+++ landscape/random_scatter.gd
@@ -1,10 +1,12 @@
 extends MultiMeshInstance
 
+var rng = RandomNumberGenerator.new()
 
 func _ready():
+	rng.seed = 0
 	for idx in multimesh.instance_count:
 		var transform = Transform(Basis.IDENTITY, Vector3.ZERO)
-		transform = transform.scaled(Vector3.ONE * rand_range(0.35, 1.5))
-		transform = transform.translated(Vector3(rand_range(-2000, 2000), 0, rand_range(-2000, 2000)))
-		transform = transform.rotated(Vector3.UP, randf())
+		transform = transform.scaled(Vector3.ONE * rng.randf_range(0.75, 2.0))
+		transform = transform.translated(Vector3(rng.randf_range(-2000, 2000), 0, rng.randf_range(-2000, 2000)))
+		transform = transform.rotated(Vector3.UP, rng.randf())
 		multimesh.set_instance_transform(idx, transform)
