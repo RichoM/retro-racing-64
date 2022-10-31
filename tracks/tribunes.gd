17295a17b560b118fe5acf49292952c9b845efb4
RichoM <richi.moran@gmail.com>
2022-10-31 16:54:43 -0300

    Hack to prevent framerate drop in mobile

--- tracks/tribunes.gd
+++ tracks/tribunes.gd
@@ -7,8 +7,16 @@ var time = 0
 
 func _ready():
 	rng.randomize()
-	for c in get_children():
-		initial_positions.append(c.translation.y)
+	
+	# HACK(Richo): In my mobile phone the framerate drops significantly when having
+	# the people in the tribunes. I don't have much time to optimize this before the
+	# deadline so I decided to simply remove them if I detect the touchscreen hint.
+	if OS.has_touchscreen_ui_hint():
+		for c in get_children():
+			remove_child(c)
+	else:
+		for c in get_children():
+			initial_positions.append(c.translation.y)
 	
 func _process(delta):
 	time += delta
